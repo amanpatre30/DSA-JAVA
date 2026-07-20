d35428c78205a5a9a04fd41e04bd6c152b745fa8
amanpatre30 <148451280+amanpatre30@users.noreply.github.com>
2026-07-20 14:17:10 +0530

    Time: 7 ms (26.16%), Space: 46.3 MB (89.24%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -1,29 +1,40 @@
 class Solution {
     public int[] separateDigits(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        int n = nums.length;
 
-        for(int num : nums){
-            int digit = num;
-            if(num < 10){
-                ans.add(digit);
-            }else{
+        // Stores the final answer
+        List<Integer> ans = new ArrayList<>();
+
+        // Traverse every number
+        for (int num : nums) {
+
+            // If the number has only one digit
+            if (num < 10) {
+                ans.add(num);
+            } else {
+
+                // Store digits temporarily
                 Stack<Integer> stack = new Stack<>();
 
-                while(num > 0){
+                // Extract digits from right to left
+                while (num > 0) {
                     stack.push(num % 10);
-                    num = num / 10;
+                    num /= 10;
                 }
 
-                while(!stack.isEmpty()){
+                // Pop to get digits in left-to-right order
+                while (!stack.isEmpty()) {
                     ans.add(stack.pop());
                 }
             }
         }
-        int result[] = new int[ans.size()];
-        for(int i =0 ; i < ans.size() ; i++){
+
+        // Convert List<Integer> to int[]
+        int[] result = new int[ans.size()];
+
+        for (int i = 0; i < ans.size(); i++) {
             result[i] = ans.get(i);
         }
+
         return result;
     }
 }
