c2a59017dbf860bdf700931ae642c0777689c095
amanpatre30 <148451280+amanpatre30@users.noreply.github.com>
2026-09-20 23:58:58 +0530

    Time: 0 ms (100%), Space: 48.4 MB (10.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,23 +1,24 @@
 class Solution {
 
-    public double power(double x, long n) {
-
-        if (n == 0) {
+    public double power(double x , long n){
+        // n become 0 in that case we return 1 because if any base to the power is 1 then it is always 1;
+        if(n == 0){
             return 1;
         }
-
-        if (n < 0) {
-            return 1 / power(x, -n);
+        //if power is odd , then seperate the base and set power to even
+        if( n % 2 == 1){
+            return x * power(x , n - 1);
         }
 
-        if (n % 2 == 1) {
-            return x * power(x, n - 1);
-        }
-
-        return power(x * x, n / 2);
+        return power(x * x , n / 2);
     }
 
     public double myPow(double x, int n) {
-        return power(x, (long)n);
+         if (n < 0) {
+            // Calculate the power of -n and take reciprocal
+            return (1.0 / power(x, -(long)n));
+        }
+
+        return power(x , (long)n);
     }
 }
